d53448d2edb31d2f635dce413238de44d07ca99d
Anastasiia1720 <163123780+Anastasiia1720@users.noreply.github.com>
2024-09-30 15:03:33 +0300

    commit

---
+++ LAB_3.8/LAB_3.8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть перше число: ");
+        int number1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Введіть друге число: ");
+        int number2 = int.Parse(Console.ReadLine());
+
+        int gcd = FindGCD(number1, number2);
+
+        Console.WriteLine($"Найбільший спільний дільник для {number1} і {number2} = {gcd}");
+    }
+
+    static int FindGCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
