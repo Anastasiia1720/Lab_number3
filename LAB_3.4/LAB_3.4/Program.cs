00ef234f13f876d7301c51dc6024fbb796169452
Anastasiia1720 <163123780+Anastasiia1720@users.noreply.github.com>
2024-09-30 14:54:14 +0300

    commit

---
+++ LAB_3.4/LAB_3.4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть число для обчислення факторіала: ");
+        int number = int.Parse(Console.ReadLine());
+
+        long factorial = 1;
+
+        for (int i = 1; i <= number; i++)
+        {
+            factorial *= i;
+        }
+
+        Console.WriteLine($"Факторіал числа {number} = {factorial}");
+    }
+}
+
