93d1f4e43c18aaf88f7bceea06472e0f92895af4
Anastasiia1720 <163123780+Anastasiia1720@users.noreply.github.com>
2024-09-30 14:51:40 +0300

    commit

---
+++ LAB_3.3/LAB_3.3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Таблиця множення для числа {number}:");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{number} * {i} = {number * i}");
+        }
+    }
+}
