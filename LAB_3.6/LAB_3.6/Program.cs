56343316b30b921eddd0ab45d661b49637789003
Anastasiia1720 <163123780+Anastasiia1720@users.noreply.github.com>
2024-09-30 14:59:10 +0300

    commit

---
+++ LAB_3.6/LAB_3.6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть число для переведення в двійкову систему: ");
+        int number = int.Parse(Console.ReadLine());
+
+        string binary = Convert.ToString(number, 2);
+
+        Console.WriteLine($"Число {number} в двійковій системі: {binary}");
+    }
+}
+
