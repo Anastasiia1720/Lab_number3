bc8e03c713c085afd5b1c736428c1619598cef63
Anastasiia1720 <163123780+Anastasiia1720@users.noreply.github.com>
2024-09-30 15:01:29 +0300

    commit

---
+++ LAB_3.7/LAB_3.7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть число: ");
+        int limit = int.Parse(Console.ReadLine());
+
+        int a = 0, b = 1, sum = 0, count = 0;
+
+        while (a < limit)
+        {
+            sum += a;
+            count++;
+
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+
+        Console.WriteLine($"Сума чисел Фібоначчі, менших за {limit}: {sum}");
+        Console.WriteLine($"Кількість елементів: {count}");
+    }
+}
+
